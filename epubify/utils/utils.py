642ab8524e2f07ddf5a023f2a3976109f8d9e1d5
Anelia Dimitrova <adimitrova@users.noreply.github.com>
2020-07-29 12:22:02 +0200

    code black formatting (#16)
    Co-authored-by: adimi5 <anelia.dimitrova@nike.com>

--- epubify/utils/utils.py
+++ epubify/utils/utils.py
@@ -5,10 +5,13 @@ import logging
 
 
 def system_import(sys, **config):
-    module_name = 'drop_box' if sys == 'dropbox' else sys.lower()
+    module_name = "drop_box" if sys == "dropbox" else sys.lower()
     try:
         from importlib import import_module
-        class_ = getattr(import_module("epubify.systems.%s" % module_name), sys.capitalize())
+
+        class_ = getattr(
+            import_module("epubify.systems.%s" % module_name), sys.capitalize()
+        )
         system_instance = class_(**config)
     except ImportError as e:
         print(e)
@@ -22,28 +25,28 @@ def set_global(key, value):
 
 def append_keyvalue_to_json_file(file_path, key, value):
     # Modify the original config file with pocket auth code once authenticated, to avoid re-authentication
-        with open(file_path, 'r') as cfi:
-            config_file_content = json.load(cfi)
-            config_file_content[key] = value
+    with open(file_path, "r") as cfi:
+        config_file_content = json.load(cfi)
+        config_file_content[key] = value
 
-        # create randomly named temporary file to avoid
-        # interference with other thread/asynchronous request
-        tempfile = path.join(path.dirname(file_path), str(uuid4()))
-        with open(tempfile, 'w') as cfo:
-            json.dump(config_file_content, cfo, indent=4)
+    # create randomly named temporary file to avoid
+    # interference with other thread/asynchronous request
+    tempfile = path.join(path.dirname(file_path), str(uuid4()))
+    with open(tempfile, "w") as cfo:
+        json.dump(config_file_content, cfo, indent=4)
 
-        # rename temporary file replacing old file
-        rename(tempfile, file_path)
+    # rename temporary file replacing old file
+    rename(tempfile, file_path)
 
 
 def read_txt(file_path):
-    with open(file_path, 'r') as file:
+    with open(file_path, "r") as file:
         text = file.read()
     return text
 
 
 def read_json(file_path, key_name=None):
-    with open(file_path, 'r') as file:
+    with open(file_path, "r") as file:
         content = json.load(file)
     if not key_name:
         return content
