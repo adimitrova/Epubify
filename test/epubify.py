eb4d79b1535111bbd757fb3e6abcdfd657be6452
adimitrova <a.dimitrovaa@gmail.com>
2020-08-21 15:58:17 +0200

    first passing test yay + test txt file

--- test/epubify.py
+++ test/epubify.py
@@ -19,16 +19,17 @@ configs = {
                 "content": utils.read_txt("test/data/how_to_dismantle_a_democracy_the_case_of_bulgaria.txt"),
                 "author": "openDemocracy"
             }
-
     },
     "pocket_to_local": {
-        "from": {
-            "system": "pocket"
-        },
-        "to": {
-            "mode": "local"
-        }
-    },
+        'article': {'author': 'epubify',
+                    'title': 'Как кокичето стана лекарство',
+                    'url': 'https://www.obekti.bg/nauka/kak-kokicheto-stana-lekarstvo'},
+        'credsFileName': 'my_keys.json',
+        'from': {'system': 'pocket'},
+        'to': {'filePath': '/Stuff/epubify',
+               'mode': 'remote',
+               'saveMode': 'overwrite',
+               'system': 'dropbox'}},
     "pocket_to_dropbox": {
         "from": {
             "system": "pocket"
@@ -46,15 +47,16 @@ configs = {
 
 class TestEpubify(unittest.TestCase):
     def setUp(self):
+        print("Creating epubify objects")
         self.epubify1 = epubify.Epubify(**configs.get("txt_to_local"))
         self.epubify2 = epubify.Epubify(**configs.get("pocket_to_local"))
         self.epubify3 = epubify.Epubify(**configs.get("pocket_to_dropbox"))
 
     def testFileName(self):
+        print("Test %s" % "testFileName")
         actual = self.epubify1.file_path
         expected = getcwd() + "/epubify/books/how_to_dismantle_a_democracy.epub"
         self.assertEqual(actual, expected)
-
 
 
 if __name__ == '__main__':
